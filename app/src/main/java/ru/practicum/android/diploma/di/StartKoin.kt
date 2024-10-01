0a4077e3902f1019de12b1b654977bcd8cc93cbf
kamil <why2man4@gmail.com>
2024-10-01 23:42:49 +0300

    small changes

--- app/src/main/java/ru/practicum/android/diploma/di/StartKoin.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/StartKoin.kt
@@ -8,6 +8,7 @@ import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.search.data.network.HHApi
 import ru.practicum.android.diploma.search.data.network.NetworkClient
 import ru.practicum.android.diploma.search.data.network.RetrofitNetworkClient
@@ -15,7 +16,7 @@ import ru.practicum.android.diploma.search.data.network.RetrofitNetworkClient
 val startModule = module{
     single<HHApi> {
         Retrofit.Builder()
-            .baseUrl("https://api.hh.ru/")
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(HHApi::class.java)
