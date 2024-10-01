40a9ccd65a26fc0662236c387f77e4703e06fa6a
kamil <why2man4@gmail.com>
2024-10-01 23:38:16 +0300

    added Koin and App

---
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import ru.practicum.android.diploma.di.startModule
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidContext(this@App)
+            modules(startModule)
+        }
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/di/StartKoin.kt
@@ -0,0 +1,35 @@
+package ru.practicum.android.diploma.di
+
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.search.data.network.HHApi
+import ru.practicum.android.diploma.search.data.network.NetworkClient
+import ru.practicum.android.diploma.search.data.network.RetrofitNetworkClient
+
+val startModule = module{
+    single<HHApi> {
+        Retrofit.Builder()
+            .baseUrl("https://api.hh.ru/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HHApi::class.java)
+    }
+
+    single {
+        Gson()
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(api = get(), context = androidContext(), ioDispatcher = get(named("ioDispatcher")))
+    }
+
+    single<CoroutineDispatcher>(named("ioDispatcher")) {
+        Dispatchers.IO
+    }
+}
