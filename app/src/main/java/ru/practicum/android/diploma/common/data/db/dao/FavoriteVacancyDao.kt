1b2a59e6a9798978d28716738c50e557c532b4db
Danil <danil377377@proton.me>
2024-10-01 23:20:28 +0300

    функции дао возвращают флоу

--- app/src/main/java/ru/practicum/android/diploma/common/data/db/dao/FavoriteVacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/db/dao/FavoriteVacancyDao.kt
@@ -4,16 +4,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.common.data.db.entity.FavoriteVacancyEntity
 
 @Dao
 interface FavoriteVacancyDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTrack(track: FavoriteVacancyEntity)
-
     @Query("SELECT * FROM favorite_vacancy_table")
-    suspend fun getTracks(): List<FavoriteVacancyEntity>
-
+    suspend fun getTracks(): Flow<List<FavoriteVacancyEntity>>
     @Query("SELECT id FROM favorite_vacancy_table")
-    fun getFavoritesIds(): List<String>
+    fun getFavoritesIds(): Flow<List<String>>
 }
