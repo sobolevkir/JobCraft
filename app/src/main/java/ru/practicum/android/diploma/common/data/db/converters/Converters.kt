979ddaf64c1a7258daeeb7989ce3d0e317004c8c
Danil <danil377377@proton.me>
2024-10-01 16:22:56 +0300

    Добавил заглушку в конвертер, чтобы сборка прошла

--- app/src/main/java/ru/practicum/android/diploma/common/data/db/converters/Converters.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/db/converters/Converters.kt
@@ -1,3 +1,10 @@
 package ru.practicum.android.diploma.common.data.db.converters
 
-class Converters
+import androidx.room.TypeConverter
+
+class Converters {
+    @TypeConverter
+    fun testConverter(text: String): String {
+        return text + ""
+    } // это конвертер-заглушка
+}
