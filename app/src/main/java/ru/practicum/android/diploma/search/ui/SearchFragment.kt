772d1f3ae59e767605734d8724a5e8dc02a78762
kamil <why2man4@gmail.com>
2024-10-06 10:18:23 +0300

    Логика появления кнопки очистки текста

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -1,7 +1,10 @@
 package ru.practicum.android.diploma.search.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -11,6 +14,7 @@ import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.search.domain.api.VacancyOnClicked
 import ru.practicum.android.diploma.search.presentation.SearchViewModel
 import ru.practicum.android.diploma.search.ui.adapter.SearchAdapter
+
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
     private val binding by viewBinding(FragmentSearchBinding::bind)
@@ -32,6 +36,29 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         viewModel.getSearchRes().observe(requireActivity()) {
             adapter.submitList(it.vacancies)
         }
+
+        binding.etSearchRequest.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {}
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                if(binding.etSearchRequest.text.isEmpty()){
+                    binding.ivClearRequest.isVisible = false
+                    binding.ivSearch.isVisible = true
+                }
+                else{
+                    binding.ivClearRequest.isVisible = true
+                    binding.ivSearch.isVisible = false
+                }
+            }
+        })
 
         binding.btnFilters.setOnClickListener {
             openFilters()
