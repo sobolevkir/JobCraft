925f76602f43cbebed01477f2413f9edd13f22be
A2T <luidor-arb@yandex.ru>
2024-10-10 00:09:31 +0400

    Восстановил Resource

--- app/src/main/java/ru/practicum/android/diploma/common/util/Resource.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/util/Resource.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.common.util
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+import ru.practicum.android.diploma.common.domain.model.ErrorType
+
+sealed class Resource<T>(val data: T? = null, val errorType: ErrorType? = null) {
     class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Error<T>(errorType: ErrorType, data: T? = null) : Resource<T>(data, errorType)
 }
