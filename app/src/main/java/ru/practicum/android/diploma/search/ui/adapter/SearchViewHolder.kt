20d998a734b827ee28692f637b4a8963753a3d74
kamil <why2man4@gmail.com>
2024-10-09 23:01:02 +0300

    Прака логики

--- app/src/main/java/ru/practicum/android/diploma/search/ui/adapter/SearchViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/adapter/SearchViewHolder.kt
@@ -10,13 +10,13 @@ import ru.practicum.android.diploma.databinding.VacancyListItemBinding
 class SearchViewHolder(private val binding: VacancyListItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    private val radius: Float = 2 * itemView.resources.displayMetrics.density
+    private val cornerRadius = binding.root.resources.getDimensionPixelSize(R.dimen.radius_small)
     fun bind(model: VacancyFromList) = with(binding) {
         tvVacancyName.text = model.name
         tvEmployment.text = model.areaName
         tvSalary.text = model.salary ?: "Зарплата не указана"
         Glide.with(itemView).load(model.employerLogoUrl240).centerCrop()
-            .transform(RoundedCorners(radius.toInt())).placeholder(
+            .transform(RoundedCorners(cornerRadius)).placeholder(
                 R.drawable.ic_cover_placeholder
             ).into(ivVacancyCover)
     }
