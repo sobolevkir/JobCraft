02cfb8ad11ce373866420ff513524a451726373c
kamil <why2man4@gmail.com>
2024-10-09 22:57:37 +0300

    Прака логики

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/SearchViewModel.kt
@@ -55,7 +55,6 @@ class SearchViewModel(private val interactor: VacanciesInteractor) : ViewModel()
             if (isSearch) {
                 isSearch = false
                 searchRequest(request, paddingPage)
-                bind(SearchState.Loading)
             }
         }
     }
@@ -67,20 +66,21 @@ class SearchViewModel(private val interactor: VacanciesInteractor) : ViewModel()
                 "page" to page.toString(),
                 "per_page" to "20"
             )
+            renderState(SearchState.Loading)
             interactor.searchVacancies(options)
                 .onEach { (searchResult, errorType) ->
                     when (errorType) {
                         null -> {
                             fullList += searchResult!!.items
-                            bind(SearchState.SearchResult(fullList, searchResult.found))
+                            renderState(SearchState.SearchResult(fullList, searchResult.found))
                             maxPage = searchResult.pages
                         }
 
-                        ErrorType.CONNECTION_PROBLEM -> bind(SearchState.InternetError)
+                        ErrorType.CONNECTION_PROBLEM -> renderState(SearchState.InternetError)
 
-                        ErrorType.NOTHING_FOUND -> bind(SearchState.NothingFound)
+                        ErrorType.NOTHING_FOUND -> renderState(SearchState.NothingFound)
 
-                        else -> bind(SearchState.ServerError)
+                        else -> renderState(SearchState.ServerError)
                     }
                     isNextPageLoading = false
                 }
@@ -88,7 +88,7 @@ class SearchViewModel(private val interactor: VacanciesInteractor) : ViewModel()
         }
     }
 
-    private fun bind(state: SearchState) {
+    private fun renderState(state: SearchState) {
         stateLiveData.postValue(state)
     }
 
