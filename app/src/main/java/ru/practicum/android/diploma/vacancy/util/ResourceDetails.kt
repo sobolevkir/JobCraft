e27aad788f6ab908a42081d81612c4cc8efc8802
A2T <luidor-arb@yandex.ru>
2024-10-09 23:42:08 +0400

    Исправление ошибок

--- app/src/main/java/ru/practicum/android/diploma/vacancy/util/ResourceDetails.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/util/ResourceDetails.kt
@@ -3,6 +3,6 @@ package ru.practicum.android.diploma.vacancy.util
 import ru.practicum.android.diploma.common.domain.model.ErrorType
 
 sealed class ResourceDetails<T>(val data: T? = null, val errorType: ErrorType? = null) {
-    class Success<T>(data: T): ResourceDetails<T>(data)
-    class Error<T>(errorType: ErrorType): ResourceDetails<T>(null, errorType)
+    class Success<T>(data: T) : ResourceDetails<T>(data)
+    class Error<T>(errorType: ErrorType) : ResourceDetails<T>(null, errorType)
 }
