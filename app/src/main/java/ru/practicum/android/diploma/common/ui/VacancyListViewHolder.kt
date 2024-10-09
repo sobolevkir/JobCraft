387acc74b4e365f2127f03b6ab8d525868e62022
Danil <danil377377@proton.me>
2024-10-09 18:58:03 +0300

    финал

--- app/src/main/java/ru/practicum/android/diploma/common/ui/VacancyListViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/ui/VacancyListViewHolder.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.common.ui
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.common.domain.model.VacancyFromList
 import ru.practicum.android.diploma.databinding.VacancyListItemBinding
