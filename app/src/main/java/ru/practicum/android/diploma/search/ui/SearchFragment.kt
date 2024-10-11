5d4f4d3477b6908ae887a9bea9205131a14acd58
sobolevkir <sobolevkir@bk.ru>
2024-10-11 12:28:43 +0300

    Detekt formatting

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -76,11 +76,9 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
 
     private fun initQueryChangeListener() {
         binding.etSearchRequest.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable) {
-            }
+            override fun afterTextChanged(s: Editable) {}
 
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 setStartOptions(s.isEmpty())
