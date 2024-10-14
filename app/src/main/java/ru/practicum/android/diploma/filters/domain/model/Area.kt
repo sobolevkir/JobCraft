a288358d2a67d71513ecbc9bfa4d1731848091b8
sobolevkir <sobolevkir@bk.ru>
2024-10-14 18:41:30 +0300

    Доработана навигация с передачей аргументов настроек фильтрации.

--- app/src/main/java/ru/practicum/android/diploma/filters/domain/model/Area.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/domain/model/Area.kt
@@ -1,7 +1,9 @@
 package ru.practicum.android.diploma.filters.domain.model
 
+import java.io.Serializable
+
 class Area(
     val parentId: String? = null,
     val id: String,
     val name: String
-)
+): Serializable
