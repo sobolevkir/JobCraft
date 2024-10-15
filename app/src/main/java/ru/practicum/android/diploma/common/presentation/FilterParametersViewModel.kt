fa5fa6b3ce07da1cb27fe4d024013a1f31faca23
sobolevkir <sobolevkir@bk.ru>
2024-10-15 15:57:43 +0300

    Добавлены методы для обновления значений отдельных полей FilterParameters.

--- app/src/main/java/ru/practicum/android/diploma/common/presentation/FilterParametersViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/presentation/FilterParametersViewModel.kt
@@ -3,7 +3,9 @@ package ru.practicum.android.diploma.common.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.filters.domain.model.Area
 import ru.practicum.android.diploma.filters.domain.model.FilterParameters
+import ru.practicum.android.diploma.filters.domain.model.Industry
 
 class FilterParametersViewModel : ViewModel() {
 
@@ -19,6 +21,46 @@ class FilterParametersViewModel : ViewModel() {
 
     fun setFilterParametersLiveData(parameters: FilterParameters) {
         filterParametersLiveData.postValue(parameters)
+    }
+
+    fun setRegion(region: Area) {
+        with(filterParametersLiveData) {
+            this.postValue(
+                this.value?.copy(region = region)
+            )
+        }
+    }
+
+    fun setCountry(country: Area) {
+        with(filterParametersLiveData) {
+            this.postValue(
+                this.value?.copy(country = country)
+            )
+        }
+    }
+
+    fun setIndustry(industry: Industry) {
+        with(filterParametersLiveData) {
+            this.postValue(
+                this.value?.copy(industry = industry)
+            )
+        }
+    }
+
+    fun setExpectedSalary(salary: Int) {
+        with(filterParametersLiveData) {
+            this.postValue(
+                this.value?.copy(expectedSalary = salary)
+            )
+        }
+    }
+
+    fun setOnlyWithSalary(onlyWithSalary: Boolean) {
+        with(filterParametersLiveData) {
+            this.postValue(
+                this.value?.copy(onlyWithSalary = onlyWithSalary)
+            )
+        }
     }
 
 }
