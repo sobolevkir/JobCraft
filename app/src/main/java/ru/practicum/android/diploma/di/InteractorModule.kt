1bc0e51e839990cfc883cb8d8e445ee273481051
sobolevkir <sobolevkir@bk.ru>
2024-10-15 18:07:13 +0300

    Реализовал поиск с учётом фильтрации. Также реализовано выполнение условия, если фильтры поменялись, то обновить поисковый запрос с учётом фильтров.

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -4,8 +4,10 @@ import org.koin.dsl.module
 import ru.practicum.android.diploma.favorites.domain.FavoritesInteractor
 import ru.practicum.android.diploma.favorites.domain.impl.FavoritesInteractorImpl
 import ru.practicum.android.diploma.filters.domain.AreaInteractor
+import ru.practicum.android.diploma.filters.domain.FiltersLocalInteractor
 import ru.practicum.android.diploma.filters.domain.IndustryInteractor
 import ru.practicum.android.diploma.filters.domain.impl.AreaInteractorImpl
+import ru.practicum.android.diploma.filters.domain.impl.FiltersLocalInteractorImpl
 import ru.practicum.android.diploma.filters.domain.impl.IndustryInteractorImpl
 import ru.practicum.android.diploma.search.domain.VacanciesInteractor
 import ru.practicum.android.diploma.search.domain.impl.VacanciesInteractorImpl
@@ -19,5 +21,5 @@ val interactorModule = module {
     factory<VacancyDetailsInteractor> { VacancyDetailsInteractorImpl(repository = get()) }
     factory<IndustryInteractor> { IndustryInteractorImpl(repository = get()) }
     factory<AreaInteractor> { AreaInteractorImpl(repository = get()) }
-
+    factory<FiltersLocalInteractor> { FiltersLocalInteractorImpl(repository = get()) }
 }
