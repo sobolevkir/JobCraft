4fb258aec291aa933cf8cd0754017b1198a20fb9
kamil <why2man4@gmail.com>
2024-10-16 19:42:59 +0300

    Сохранение выбранной отрасли

--- app/src/main/java/ru/practicum/android/diploma/filters/presentation/IndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/presentation/IndustryViewModel.kt
@@ -44,7 +44,7 @@ class IndustryViewModel(private val interactor: IndustryInteractor) : ViewModel(
     private fun processingResult(industry: List<Industry>?, errorType: ErrorType?) {
         if (industry != null) {
             searchedIndustries = industry
-            renderState(FilterIndustryState.IndustryFound(sortAreas(industry)))
+            renderState(FilterIndustryState.IndustryFound(sortIndustries(industry)))
         } else {
             when (errorType) {
                 ErrorType.NOTHING_FOUND -> {
@@ -66,7 +66,7 @@ class IndustryViewModel(private val interactor: IndustryInteractor) : ViewModel(
         industryState.postValue(state)
     }
 
-    private fun sortAreas(area: List<Industry>): List<Industry> {
+    private fun sortIndustries(area: List<Industry>): List<Industry> {
         val sortedListByName = area.sortedBy { it.name.replace('Ё', 'Е').replace('ё', 'е') }
         val areasWithoutDigits = sortedListByName.filter { !it.name.any { char -> char.isDigit() } }
         val areasWithDigits = sortedListByName.filter { it.name.any { char -> char.isDigit() } }
