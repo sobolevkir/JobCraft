576d2743fc5520b52c6f57959bdee30a2901a062
A2T <luidor-arb@yandex.ru>
2024-10-16 18:00:37 +0400

    Экран Место работы

---
+++ app/src/main/java/ru/practicum/android/diploma/filters/presentation/PlaceState.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.filters.presentation
+
+data class PlaceState(
+    val country: String?,
+    val region: String?
+)
--- app/src/main/java/ru/practicum/android/diploma/filters/presentation/PlaceViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/presentation/PlaceViewModel.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.filters.presentation
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,42 +8,13 @@ import ru.practicum.android.diploma.filters.domain.model.Area
 
 class PlaceViewModel : ViewModel() {
 
-    private var currentCountry: Area? = null
-    private var currentRegion: Area? = null
     private var areaLiveData = MutableLiveData(PlaceState(null, null))
 
     fun getAreaLiveData(): LiveData<PlaceState> = areaLiveData
 
     fun passNewParameters(country: Area?, region: Area?) {
-        var strCountry: String? = null
-        var strRegion: String? = null
-        if (country != currentCountry) {
-            currentCountry = country
-            strCountry = country?.name
-        }
-        if (region != currentRegion) {
-            currentRegion = region
-            strRegion = region?.name
-        }
-        areaLiveData.postValue(PlaceState(strCountry, strRegion))
-    }
-
-    fun resetCountry() {
-        if (currentCountry != null) {
-            currentCountry = null
-            areaLiveData.postValue(PlaceState(null, currentRegion?.name))
-        }
-    }
-
-    fun resetRegion() {
-        if (currentRegion != null) {
-            currentRegion = null
-            areaLiveData.postValue(PlaceState(currentCountry?.name, null))
-        }
+        areaLiveData.postValue(PlaceState(country?.name, region?.name))
+        //Log.e ("МОЁ", country?.name + "  " + region?.name)
     }
 }
 
-data class PlaceState(
-    val country: String?,
-    val region: String?
-)
