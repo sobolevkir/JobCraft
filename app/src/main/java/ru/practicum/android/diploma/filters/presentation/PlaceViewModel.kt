90357335a1b86507e3ea03438598633fd3b3555e
A2T <luidor-arb@yandex.ru>
2024-10-16 18:12:21 +0400

    Экран Место работы Исправление ошибок

--- app/src/main/java/ru/practicum/android/diploma/filters/presentation/PlaceViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/presentation/PlaceViewModel.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.filters.presentation
 
-// import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
