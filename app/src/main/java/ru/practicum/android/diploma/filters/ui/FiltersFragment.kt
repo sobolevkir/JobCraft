394dbd3bb0d56c062ab49161a7c09a49944fc244
sobolevkir <sobolevkir@bk.ru>
2024-10-16 18:48:52 +0300

    Detekt formatting

--- app/src/main/java/ru/practicum/android/diploma/filters/ui/FiltersFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/ui/FiltersFragment.kt
@@ -91,8 +91,12 @@ class FiltersFragment : Fragment(R.layout.fragment_filters) {
                     }
                 }
 
-                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                    //Empty
+                }
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    //Empty
+                }
             })
             etSelectPlace.addTextChangedListener(
                 CustomTextWatcher(tlSelectPlace) {
