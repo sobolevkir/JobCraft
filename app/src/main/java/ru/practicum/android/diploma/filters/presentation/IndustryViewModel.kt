ddde0a1db65ea97717f074852188e84d6c8a88d6
kamil <why2man4@gmail.com>
2024-10-20 20:24:15 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/filters/presentation/IndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/presentation/IndustryViewModel.kt
@@ -12,8 +12,10 @@ import ru.practicum.android.diploma.filters.domain.model.Industry
 import ru.practicum.android.diploma.filters.presentation.models.FilterIndustryState
 import ru.practicum.android.diploma.filters.ui.IndustryForUi
 
-class IndustryViewModel(private val interactor: IndustryInteractor,
-                        private val converter: IndustryConverter) : ViewModel() {
+class IndustryViewModel(
+    private val interactor: IndustryInteractor,
+    private val converter: IndustryConverter
+) : ViewModel() {
 
     private val industryState = MutableLiveData<FilterIndustryState>()
     private var selectedId = "-1"
