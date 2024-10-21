2aea21cfab6020c34a59416bc7e28ce3e14297ef
kamil <why2man4@gmail.com>
2024-10-21 15:39:31 +0300

    IndustriesViewHolder вынесен в отдельны файл

--- app/src/main/java/ru/practicum/android/diploma/filters/ui/adapters/IndustriesAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/ui/adapters/IndustriesAdapter.kt
@@ -3,29 +3,12 @@ package ru.practicum.android.diploma.filters.ui.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.databinding.IndustryListItemBinding
 import ru.practicum.android.diploma.filters.ui.IndustryForUi
 
 class IndustriesAdapter(
     private val onItemSelect: (IndustryForUi) -> Unit
-) : ListAdapter<IndustryForUi, IndustriesAdapter.IndustriesViewHolder>(IndustryItemComparator()) {
-
-    inner class IndustriesViewHolder(private val binding: IndustryListItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(model: IndustryForUi) {
-            with(binding.rbIndustryItemSelect) {
-                text = model.name
-                isChecked = model.isSelected
-            }
-            binding.industryListItem.setOnClickListener {
-                if (!model.isSelected) {
-                    onItemSelect(model)
-                }
-            }
-        }
-    }
+) : ListAdapter<IndustryForUi, IndustriesViewHolder>(IndustryItemComparator()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IndustriesViewHolder {
         val layoutInspector = LayoutInflater.from(parent.context)
@@ -34,6 +17,6 @@ class IndustriesAdapter(
 
     override fun onBindViewHolder(holder: IndustriesViewHolder, position: Int) {
         val item = getItem(position)
-        holder.bind(item)
+        holder.bind(item, onItemSelect)
     }
 }
